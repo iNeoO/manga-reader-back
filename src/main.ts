aa577d0576f1f6dbb1e36c190e57f1470ee67298
valère pique <valere@harfanglab.fr>
2022-02-16 18:40:40 +0100

    feat: fix migration fastify

--- src/main.ts
+++ src/main.ts
@@ -5,17 +5,19 @@ import {
 } from '@nestjs/platform-fastify';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
-import * as cookieParser from 'cookie-parser';
 import * as compression from 'compression';
+import fastifyCookie from 'fastify-cookie';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
-    new FastifyAdapter(),
+    new FastifyAdapter({ ignoreTrailingSlash: true }),
   );
   app.setGlobalPrefix('api');
-  app.use(cookieParser());
   app.use(compression());
+  app.register(fastifyCookie, {
+    secret: process.env.COOKIE_SIGNATURE,
+  });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
--- src/page/page.controller.ts
+++ src/page/page.controller.ts
@@ -2,11 +2,10 @@ import {
   Controller,
   Get,
   Param,
-  StreamableFile,
   NotFoundException,
   UseGuards,
 } from '@nestjs/common';
-import { createReadStream } from 'fs';
+import { createReadStream, ReadStream } from 'fs';
 import * as sharp from 'sharp';
 import { join } from 'path';
 import { PageService } from './page.service';
@@ -18,25 +17,27 @@ export class PageController {
 
   @UseGuards(JwtAuthGuard)
   @Get(':id')
-  async findOne(@Param('id') id: string): Promise<StreamableFile> {
+  async findOne(@Param('id') id: string): Promise<ReadStream> {
     const page = await this.pageService.getPage({ id });
     try {
       const file = createReadStream(join(process.env.CDN_PATH, page.path));
-      return new StreamableFile(file);
+      return file;
     } catch (error) {
       throw new NotFoundException('Image not found.');
     }
   }
 
   @Get(':id/minified')
-  async findOneMinified(@Param('id') id: string): Promise<StreamableFile> {
+  async findOneMinified(@Param('id') id: string): Promise<Buffer> {
     const page = await this.pageService.getPage({ id });
     try {
-      const file = await sharp(join(process.env.CDN_PATH, page.path))
+      const file = await sharp(join(process.env.CDN_PATH, page.path), {
+        failOnError: false,
+      })
         .resize(50)
         .png()
         .toBuffer();
-      return new StreamableFile(file);
+      return file;
     } catch (error) {
       throw new NotFoundException('Image not found.');
     }
